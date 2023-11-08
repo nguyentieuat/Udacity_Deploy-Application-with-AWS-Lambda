65bfd2807fc467eaa7a316473ab92a0630374f4b
nguyentieuat <thanhluan.kma@gmail.com>
2023-11-08 13:52:18 +0700

    update

--- backend/src/lambda/http/getTodos.js
+++ backend/src/lambda/http/getTodos.js
@@ -1,4 +1,33 @@
-export function handler(event) {
-  // TODO: Get all TODO items for a current user
-  return undefined
-}
+import middy from '@middy/core'
+import cors from '@middy/http-cors'
+import httpErrorHandler from '@middy/http-error-handler'
+import { getAllTodos } from '../../businessLogic/todos.mjs'
+import { createLogger } from '../../utils/logger.mjs'
+import {getUserId} from '../utils.mjs'
+
+const logger = createLogger('http/getTodos.js')
+
+export const handler = middy()
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
+    })
+  )
+  .handler(async (event) => {
+    // TODO: Get all TODO items for a current user
+    //logger.info("Processing event: ", event)
+    logger.info(`Processing event: ${event} `, {function: "handler()"})
+
+    /* Get User ID */
+    const userId = getUserId(event)
+
+    const todos = await getAllTodos(userId)
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        items: todos
+      })
+    }
+  })

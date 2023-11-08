386f633045acd5822fe78d2df1feaa13ac01e7db
nguyentieuat <thanhluan.kma@gmail.com>
2023-11-08 20:17:21 +0700

    update

--- backend/src/fileStorage/attachmentUtils.mjs
+++ backend/src/fileStorage/attachmentUtils.mjs
@@ -1,4 +1,4 @@
-import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { PutObjectCommand, S3Client, S3 } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import { createLogger } from '../utils/logger.mjs'
 import AWSXRay from 'aws-xray-sdk-core'
@@ -6,24 +6,42 @@ import AWSXRay from 'aws-xray-sdk-core'
 const logger = createLogger('fileStorage/attachmentUtils.mjs')
 
 const s3Client = AWSXRay.captureAWSv3Client(new S3Client())
-const bucketName = process.env.ATTACHMENT_BUCKET
+const bucketName = process.env.ATTACHMENT_S3_BUCKET
 const urlExpiration = parseInt(process.env.SIGNED_URL_EXPIRATION)
 
 export async function getUploadUrl(fileId) {
-    
+
     const command = new PutObjectCommand({
-      Bucket: bucketName,
-      Key: fileId
+        Bucket: bucketName,
+        Key: fileId
     })
 
     const uploadFileUrl = await getSignedUrl(s3Client, command, {
-      expiresIn: urlExpiration
+        expiresIn: urlExpiration
     })
-    
-    logger.info(`Obtained an uploadUrl = ${uploadFileUrl}`, {function: "getUploadUrl()"} )
+
+    logger.info(`Obtained an uploadUrl = ${uploadFileUrl}`, { function: "getUploadUrl()" })
     return uploadFileUrl
 }
 
-export function getAttachmentUrl(fileId) {
+export async function getAttachmentUrl(fileId) {
+    const exists = await S3
+        .headObject({
+            Bucket: bucketName,
+            Key: fileId,
+        })
+        .promise()
+        .then(
+            () => true,
+            err => {
+                if (err.code === 'NotFound') {
+                    return false;
+                }
+                throw err;
+            }
+        );
+    if (!exists) {
+        return null;
+    }
     return `https://${bucketName}.s3.amazonaws.com/${fileId}`
 }

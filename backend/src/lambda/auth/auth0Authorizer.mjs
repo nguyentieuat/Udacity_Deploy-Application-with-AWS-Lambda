e5b4ae4f13a27466ec2b1f754b3c1c6999f46589
nguyentieuat <thanhluan.kma@gmail.com>
2023-11-08 18:18:03 +0700

    update

--- backend/src/lambda/auth/auth0Authorizer.mjs
+++ backend/src/lambda/auth/auth0Authorizer.mjs
@@ -2,6 +2,27 @@ import Axios from 'axios'
 import jsonwebtoken from 'jsonwebtoken'
 import { createLogger } from '../../utils/logger.mjs'
 
+/* Hard-coded certificate from Auth0 */
+const certificate = `-----BEGIN CERTIFICATE-----
+MIIDHTCCAgWgAwIBAgIJRUffkTft9kGzMA0GCSqGSIb3DQEBCwUAMCwxKjAoBgNV
+BAMTIWRldi1xdHVxbnd0MHZzNzhvdWV4LnVzLmF1dGgwLmNvbTAeFw0yMzExMDcw
+NzU5MjhaFw0zNzA3MTYwNzU5MjhaMCwxKjAoBgNVBAMTIWRldi1xdHVxbnd0MHZz
+NzhvdWV4LnVzLmF1dGgwLmNvbTCCASIwDQYJKoZIhvcNAQEBBQADggEPADCCAQoC
+ggEBAMQSQ7mzT+PZPhbgEtEOqcZgL80wZixmACQ7A4PZ26CN3v66sRKJneeepIxo
+FSGg4AfP1w/UO+/jYoGN2Gl2RXR1dJv5oAsm5MOwgoL6w8rPJwNcRluAmN2rX+ov
+u+63sGqlxad5w8Gapuc6GKHGMHG9Z2bTySmvG7uv+nrTS0t4bSu0VEJALRgbp7xI
+G1IQ3h7S8K6LITN1XoH/VXi6YVWcQ9QxV6VQLwQfl3zz+3envFyy2G5/APtQRU7c
+3BoqNClCqCROuhEdDJ+HUhxhmX+KAVs833alv0adJ3/DvY9VhpQy2s4hLbUQblZX
+t3wjeGAyqdxNkBrzBBw5459OmTUCAwEAAaNCMEAwDwYDVR0TAQH/BAUwAwEB/zAd
+BgNVHQ4EFgQUbRZlW/zDhYrFp0mtv218VRyEzuAwDgYDVR0PAQH/BAQDAgKEMA0G
+CSqGSIb3DQEBCwUAA4IBAQAHWa4OAmI+Dnou1VgpK+uAHo4QOzM3h6A+WfUvEJTE
+MHWZg95SlXklNs69jP9utIbeuwpwlEt0eWHM5rMte2uuLQ38+4moZXbuP/WMPvCU
+JIbleayvvxWLnykLLAPMU3yPN4DEFRtL1m2oNbRj3fHu3GxLvvzacJJXGCPstnvL
+M9xYbny6cCBweWgR6oZ9nhPQs1GPTEpgaBGXxEEqUmYdLSiZajDu45N5Xwxdof0v
+OY4Yihw6+PFNLFL/AFkldtJ76iDxgGNd0i2AZG1U0HfHKdi9/CpNkMuAA/cb+3c+
+M0xXvRIxSEbi0+f9J8X7lK1y12TM5HlMApMJYu/td10N
+-----END CERTIFICATE-----`
+
 const logger = createLogger('auth')
 
 const jwksUrl = 'https://dev-qtuqnwt0vs78ouex.us.auth0.com/.well-known/jwks.json'
@@ -50,21 +71,8 @@ async function verifyToken(authHeader) {
   const jwt = jsonwebtoken.decode(token, { complete: true })
 
   // TODO: Implement token verification
-  const response = await Axios.get(jwksUrl);
-  const keys = response.data.keys;
-  const signinKeys = keys.find(key => key.id === jwt.header.kid);
-  logger.info('signinKeys', signinKeys);
-  if(!signinKeys) {
-    throw new Error('The JWKS did not contain any keys');
-  }
-  //get pem data
-  const pemData = signinKeys.x5c[0];
-  //convert pem data to cert
-  const cert = `-----BEGIN CERTIFICATE-----\n${pemData}\n-----END CERTIFICATE-----`;
-  //verify token
-  const verifiedToken = jsonwebtoken.verify(token, cert, {algorithms: ['RS256']})
-  console.log('verifiedToken', verifiedToken);
-  return verifiedToken;
+  jsonwebtoken.verify(token, certificate, {algorithms: ['RS256']})
+  return jwt;
 }
 
 function getToken(authHeader) {
